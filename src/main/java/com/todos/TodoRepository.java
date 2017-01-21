13b1c707119a1ef52350b247a9052e4956a8a4e2
davified <davified@gmail.com>
2017-01-21 20:38:07 +0530

    Add CRUD endpoints with data-rest and data-jpa

--- src/main/java/com/todos/TodoRepository.java
+++ src/main/java/com/todos/TodoRepository.java
@@ -1,13 +1,14 @@
 package com.todos;
-import org.springframework.data.repository.PagingAndSortingRepository;
+
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
-@RepositoryRestResource(collectionResourceRel = "todos", path = "todos")
-public interface TodoRepository extends PagingAndSortingRepository<Todo, Long> {
+@RepositoryRestResource(collectionResourceRel = "todos", path = "todos") //  this is not necessary unless you are defining custom endpoints that have a different name from your model
+public interface TodoRepository extends CrudRepository<Todo, Long> {
 
   List<Todo> findByCategory(@Param("category") String category);
-
+  //  usage: http://localhost:8080/todos/search/findByCategory?category=cooking
 }
